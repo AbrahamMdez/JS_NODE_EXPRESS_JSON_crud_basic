35e928d56d1a0b1c4f404c6c14416a2511dcdf58
Abraham <nencho_1@hotmail.com>
2020-12-28 18:05:21 +0100

    Created all npm modules, file index.js and conections

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 my_second_app_CRUD
+
+This is my second practice to take knowledge about Rest Api´s, Node.js and I´m going to do Postman instead use a client interface because on this practice I only want to practice CRUD methods.
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const morgan = require("morgan");
+
+//Settings
+//Aqui le decimos que si hay un puerto en la nube que lo coja y sino hay, que se conecte a nuestro puerto 3000.
+app.set('port', process.env.PORT || 3000);
+
+//Middelwares
+app.use(morgan('dev'));
+//Este es un metodo que nos permite recibir y que el programa entienda el formato JSON.
+app.use(express.json());
+//Con esto decimos que los datos que recibamos solo sean datos Json, evitar imagenes o o otro tipo de datos
+//que sean innecesarios y pesados
+app.use(express.urlencoded({extended: false}));
+
+//Starting server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
