f92844d68ee36cc3f023bea12c604d9943475106
Abraham <nencho_1@hotmail.com>
2020-12-28 19:22:21 +0100

    Error with merge

--- routes/movies.js
+++ routes/movies.js
@@ -7,4 +7,16 @@ router.get('/movies', (req, res) => {
     res.json(Movies);
 });
 
-module.exports = router;
+router.post('/movies', (req, res) => {
+    //De esta forma almacenamos cada propiedad en una constante, para poder tenerlas individualmente
+    const { title, amounth, price } = req.body;
+    //Aqui le decimos que si alguno de los datos no es introducido lance un aviso y si esta bien, muestre datos.
+    if ( title && amounth && price ) {
+        res.send({ title, amounth, price });
+    } else {
+        res.send('Datos erroneos');
+    }
+    res.send('Received');
+});
+
+module.exports = router;  
